2acb2d399f730dd428c30bac4ce3f2914d921b67
TugiatheNarrator <tugia.narrator@gmail.com>
2025-12-13 18:22:19 -0800

    Update supers.ts

--- data/supers.ts
+++ data/supers.ts
@@ -4,6 +4,7 @@ export const supers: Array<Super> = [
   { name: 'Millennials', slug: 'millennials', threatRating: 2.1, description: 'Computer, Social media', img: 'millennials.png' },
   { name: 'Generation Z', slug: 'generation-z', threatRating: 2.5, description: 'Smartphone, Social Media,', img: 'generation-z.png' },
   { name: 'Generation Alpha', slug: 'generation-alpha', threatRating: 4.1, description: 'iPad, Social Media, AI', img: 'generation-alpha.png' },
+  { name: 'Generation Beta', slug: 'generation-beta', threatRating: 6.1, description: 'iPad, Social Media, AI', img: 'generation-beta.png' },
 ]
 
 export const supersResult: Array<SuperResult> = [
@@ -15,7 +16,7 @@ export const supersResult: Array<SuperResult> = [
       'Computer',
     lastActiveRecord: '31.12.80',
     locationKnown: true,
-    threatRating: '6.1 Exercise moderate caution',
+    threatRating: '6.7 Exercise moderate caution',
   },
   {
     name: 'Baby boomers',
@@ -25,7 +26,7 @@ export const supersResult: Array<SuperResult> = [
       'TV ',
     lastActiveRecord: '31.12.64',
     locationKnown: true,
-    threatRating: '6.7 Exercise moderate caution',
+    threatRating: '6.8 Exercise moderate caution',
   },
   {
     name: 'Silent Generation',
@@ -46,15 +47,5 @@ export const supersResult: Array<SuperResult> = [
     lastActiveRecord: '31.12.27',
     locationKnown: false,
     threatRating: '9.3 Exercise high caution',
-  },
-  {
-    name: 'Generation Beta',
-    slug: 'generation-beta',
-    img: 'generation-beta.png',
-    description:
-      'iPad, Social media, AI ',
-    lastActiveRecord: '31.12.39',
-    locationKnown: false,
-    threatRating: '6.8 Exercise moderate caution',
   },
 ]
